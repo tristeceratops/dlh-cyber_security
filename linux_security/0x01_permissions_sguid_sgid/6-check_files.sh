887be2eafb627d0c1fe92ca52f97772dde551cfb
kaliceratops <eric.woillard@gmail.com>
2026-05-11 05:25:06 -0400

    add type f

--- linux_security/0x01_permissions_sguid_sgid/6-check_files.sh
+++ linux_security/0x01_permissions_sguid_sgid/6-check_files.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo find "$1" \( -perm -4000 -o -perm -2000 \) -mtime -1 -exec ls -ldb {} \; 2>/dev/null
+sudo find "$1" \( -perm -4000 -o -perm -2000 \) -type f -mtime -1 -exec ls -ldb {} \; 2>/dev/null
