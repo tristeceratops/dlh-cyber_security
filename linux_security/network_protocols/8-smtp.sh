1b12aa7511e45cd3732b56d89b9ce9f8fbf640f3
kaliceratops <eric.woillard@gmail.com>
2026-05-24 10:00:46 -0400

    test

--- linux_security/network_protocols/8-smtp.sh
+++ linux_security/network_protocols/8-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-postconf smtpd_tls_security_level | grep -Eq '=\s*(may|encrypt)' && postconf smtpd_tls_security_level || echo "STARTTLS not configured"
+postconf smtpd_tls_security_level | grep '^smtpd_tls_security_level' | grep -Eq '=\s*(may|encrypt)' && postconf smtpd_tls_security_level || echo "STARTTLS not configured"
