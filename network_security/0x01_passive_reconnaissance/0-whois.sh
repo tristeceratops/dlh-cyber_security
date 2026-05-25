c405f175573829c48e86a194139c20d8bb326a7c
kaliceratops <eric.woillard@gmail.com>
2026-05-25 09:22:58 -0400

    ex1

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | grep -E '^(Registrant|Admin|Tech)' | awk -F ':' '/Ext/ { print $1 ":," $2 " "; next} /Street/ { print $1 "," $2; next} { print $1","$2}' > $1.csv
+whois "$1" | grep -E '^(Registrant|Admin|Tech)' | awk -F ':' '/Ext/ { print $1 ":," $2 " "; next} /Street/ { print $1 "," $2; next} { print $1","$2}' > "$1".csv
