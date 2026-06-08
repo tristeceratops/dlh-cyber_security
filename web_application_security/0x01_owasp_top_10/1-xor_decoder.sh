b8e4460d57b47061a0af9d58363147b2442baf15
kaliceratops <eric.woillard@gmail.com>
2026-06-08 04:12:33 -0400

    new day new version new try

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-    echo './1-xor_decoder.sh [xor to decode]'
-    exit 1
-fi
-
-if [[ "$1" != \{xor\}* ]]; then
-    exit 1
-fi
-
 encoded="${1#\{xor\}}"
-
-if ! decoded=$(printf '%s' "$encoded" | base64 -d 2>/dev/null); then
-    echo "Invalid Base64 input" >&2
-    exit 1
-fi
+decoded=$(printf '%s' "$encoded" | base64 -d)
 
 key=95
 i=0
 
-while [ "$i" -lt "${#decoded}" ]; do
+while [ "$i" -lt "${#decoded}" ]
+do
     byte=$(printf '%d' "'${decoded:$i:1}")
     printf "\\$(printf '%03o' "$((byte ^ key))")"
     i=$((i + 1))
 done
 
-echo
+printf '\n'
