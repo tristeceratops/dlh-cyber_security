50c3f1a8b209bd4b6c89b35c268104eb1a40e0db
Eric <eric.woillard@gmail.com>
2026-07-27 17:32:58 +0200

    wip

--- blue_team/1x04_crypto_foundation/12-luks_manager.sh
+++ blue_team/1x04_crypto_foundation/12-luks_manager.sh
@@ -18,27 +18,22 @@ case "$1" in
 create)
     echo "[+] Creating a ${SIZE}MB file to use as a virtual disk..."
 
-    # Create 500MB file using dd
     dd if=/dev/zero of="$VOLUME" bs=1M count="$SIZE" status=progress
 
     echo "[+] Formatting file with LUKS encryption..."
 
-    # Format with LUKS
     cryptsetup luksFormat "$VOLUME"
 
     echo "[+] Opening encrypted volume..."
 
-    # Open the encrypted volume
     cryptsetup luksOpen "$VOLUME" "$MAPPER"
 
     echo "[+] Creating ext4 filesystem..."
 
-    # Create filesystem
     mkfs.ext4 "/dev/mapper/$MAPPER"
 
     echo "[+] Closing encrypted volume..."
 
-    # Unmount and close
     cryptsetup luksClose "$MAPPER"
 
     echo "[+] LUKS volume created successfully."
@@ -47,15 +42,12 @@ create)
 open)
     echo "[+] Opening encrypted volume..."
 
-    # Open the encrypted volume
     cryptsetup luksOpen "$VOLUME" "$MAPPER"
 
-    # Create mount point
     mkdir -p "$MOUNT_POINT"
 
     echo "[+] Mounting encrypted filesystem..."
 
-    # Mount volume
     mount "/dev/mapper/$MAPPER" "$MOUNT_POINT"
 
     echo "[+] Volume mounted at $MOUNT_POINT"
@@ -64,12 +56,10 @@ open)
 close)
     echo "[+] Unmounting encrypted volume..."
 
-    # Unmount filesystem
     umount "$MOUNT_POINT"
 
     echo "[+] Closing LUKS volume..."
 
-    # Close encrypted volume
     cryptsetup luksClose "$MAPPER"
 
     echo "[+] Volume unmounted and closed."
