7077d71c05ac8b65a0b35b52561ae151bc4a8adb
tristeceratops <eric.woillard@gmail.com>
2026-08-10 17:17:16 +0200

    srx

--- blue_team/2x02_eyes_on_endpoint/6-log_source_map.sh
+++ blue_team/2x02_eyes_on_endpoint/6-log_source_map.sh
@@ -21,7 +21,7 @@ declare -a SOURCES=(
 )
 
 printf "%-20s %-32s %-9s %-12s %-10s %s\n" \
-    "Source" "Path" "Format" "Rotation" "Events/hr" "Relevance"
+    "Source" "Path" "Format" "Rotation" "events/hr" "Relevance"
 printf "%-20s %-32s %-9s %-12s %-10s %s\n" \
     "------" "----" "------" "--------" "---------" "---------"
 
